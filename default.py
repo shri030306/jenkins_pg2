8458a3944294878c5951353fc133eba0a7135e81
shri030306 <shrilakshmigs03@gmail.com>
2025-11-22 10:54:26 +0530

    Create default.py

---
+++ default.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv)==3:
+  script_name=sys.argv[0]
+  name-sys.argv[1]
+  rolln0=sys.argv[2]
+  print("User provided input values:")
+else:
+  script_name=sys.argv[0]
+  name="Shrilakshmi"
+  rollno="314"
+  print("NO input given using default values:")
+
+print("Script name:",script_name)
+print("Student Name:",name)
+print("Student Roll NO:",rollno)
